51322195121f88f86fbedb16585ecb3b769c01d7
Jeffrey C. Lee <leejeffc@gmail.com>
2013-07-22 02:41:10 -0700

    added more explanation

--- odin.js.project_euler/problem1.js
+++ odin.js.project_euler/problem1.js
@@ -1,21 +1,28 @@
-// Problem 1 - find sum of multiples of 3 and 5 as described here:
+// Problem 1 - Find sum of multiples of 3 and 5 as described here:
 // http://projecteuler.net/problem=1
 
 /*
-Expalanation:
+Expalanations:
 Solution 1 - Straightforward
+This solution uses a for loop to examine every integer from 1 to input-1.
+The examination is done by checking if the integer is a multiple of 3
+by using the modulus operator (%). If the integer is a multiple of 3,
+then the if statement will find that it has a remainder of 0 when
+divided by 3. Then the function adds to the sum accordingly. The same
+process is used for finding multiples of 5 in the else-if statement.
+
 Solution 2 - Shorter loop
 The second solution creates multiples rather than searching for them,
-allowing us to use a for-loop that iterates 1/3 as many times
-as the for-loop found in Solution 1. Both solutions run in a split-second,
+allowing us to use a for loop that iterates 1/3 as many times
+as the for loop found in Solution 1. Both solutions run in a split-second,
 so it doesn't really matter...unless the input is a really big number.
 Once the input reaches ~10 million, there is a noticeable difference
 in runtime (humans can easily notice solution 1 takes half a second).
 
-The first part of the if-statement checks that the multiple of 5 is
+The first part of the if statement checks that the multiple of 5 is
 still within the limit. This check does not need to be performed
-for multiples of 3 due to "i < max/3" in the for-loop condition.
-The second part of the if-statement checks if the multiple of 5
+for multiples of 3 due to "i < max/3" in the for loop condition.
+The second part of the if statement checks if the multiple of 5
 is redudant to multiples of 3. For example, we don't want to add
 "15" to the sum as a multiple of 3 and a multiple of 5. It
 just needs to be added to the sum once, not twice.
