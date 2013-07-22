c0ba215978521e78d1079b1e2e557cb1d7282f6b
Jeffrey C. Lee <leejeffc@gmail.com>
2013-07-22 14:38:54 -0700

    completed

--- odin.js.sketch_pad/script.js
+++ odin.js.sketch_pad/script.js
@@ -1,20 +1,26 @@
+var prompt_size = function(){
+	$('#container').html("");
 
-var input = 128;
+	var input = prompt('Type an integer between 1 and 128');
 
-$(document).ready(function(){
-	var body_width = $('body').width();
-	var square_size = (body_width - input*2)/input;
+	if (input >= 1 && input <= 128){
+		var body_width = $('body').width();
+		var square_size = (body_width - input*2)/input;
 
-	for(var i = 1; i <= input; i++){
-		for(var j = 1; j <= input; j++){
-			$('body').append('<div class="grid_square"></div>');
+		for(var i = 1; i <= input; i++){
+			for(var j = 1; j <= input; j++){
+				$('#container').append('<div class="grid_square"></div>');
+			}
+			$('#container').append('<div class="new_row"></div>');
 		}
-		$('body').append('<div class="new_row"></div>');
-	}
-	$('.grid_square').css('width',square_size);
-	$('.grid_square').css('height',square_size);
+		$('.grid_square').css('width',square_size);
+		$('.grid_square').css('height',square_size);
 
-	$('.grid_square').mouseenter(function(){
-		$(this).addClass('grid_square_lit');
-	});
-});
+		$('.grid_square').mouseenter(function(){
+			$(this).addClass('grid_square_lit');
+		});
+	}
+	else{
+
+	}
+}
