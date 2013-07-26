fe252454b7eed86ae48031b653a1624b3ba35253
Jeffrey C. Lee <leejeffc@gmail.com>
2013-07-26 01:00:23 -0700

    adding more options

--- odin.js.sketch_pad/script.js
+++ odin.js.sketch_pad/script.js
@@ -1,11 +1,10 @@
-var prompt_size = function(){
+var start = function(option){
 	$('#container').html("");
 
 	var input = prompt('Type an integer between 1 and 128');
 
-	if (input >= 1 && input <= 128){
-		var body_width = $('body').width();
-		var square_size = (body_width - input*2)/input;
+	if (input >= 1 && input <= 128){	// Check for non-ridiculous input
+		var square_size = $('body').width()/input - 2;	// The -2 accounts for border size
 
 		for(var i = 1; i <= input; i++){
 			for(var j = 1; j <= input; j++){
@@ -17,10 +16,25 @@ var prompt_size = function(){
 		$('.grid_square').css('height',square_size);
 
 		$('.grid_square').mouseenter(function(){
-			$(this).addClass('grid_square_lit');
+			switch(option){
+				case 1:
+					$(this).addClass('grid_square_lit');
+					break;
+				case 2:
+
+					break;
+				case 3:
+					// animate to 0% opacity over 200ms, then back to 100%
+					// opacity over 800ms when cursor leaves square
+					$(this).fadeTo(200,0);
+					$(this).mouseleave(function(){
+						$(this).fadeTo(800,1);
+					});
+					break;
+			}
 		});
 	}
 	else{
-
+		alert('No. You are crazy.');
 	}
 }
